298fee516c765402adad092a52e76b3c3c27459d
Xavier Agullo <xavier.agullo@socialpoint.es>
2019-08-23 09:46:49 +0200

    Simple loading scene and lasers aiming in the right direction

---
+++ CSS/Assets/LoadingScreenControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenControl : MonoBehaviour
+{
+    public void OnClickedPlay()
+    {
+        SceneManager.LoadScene("ReskinScene");
+    }
+}
