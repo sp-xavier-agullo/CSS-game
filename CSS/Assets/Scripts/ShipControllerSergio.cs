f4236636bab3bf1bfb05e7ecb89591eaa9bf7524
Xavier Agullo <xavier.agullo@socialpoint.es>
2019-08-23 12:00:08 +0200

    Restore ship shooting lasers

--- CSS/Assets/Scripts/ShipControllerSergio.cs
+++ CSS/Assets/Scripts/ShipControllerSergio.cs
@@ -5,110 +5,124 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.Serialization;
 
-    public class ShipControllerSergio : MonoBehaviour
+public class ShipControllerSergio : MonoBehaviour
+{
+
+    public float healthPoints;
+
+    public float speed = 50f;
+    public float turnSpeed = 0.5f;
+
+    public float hoverForce = 65f;
+    public float hoverHeight = 10f;
+
+    public float bulletSpeed = 1000;
+
+    public GameObject laserGreenBullet;
+    public GameObject playerBulletsFolder;
+
+    private float maxSpeed = 100;
+    private float minSpeed = 20;
+
+    private int activeCannon = 1;
+
+    private float powerInput;
+    private float turnInput;
+
+    public FixedJoystick fixedJoystick;
+
+    private Rigidbody shipRigidBody;
+
+    private Animator anim;
+
+    [SerializeField] Text speedText;
+    [SerializeField] Text healthText;
+    //[SerializeField] CameraScript myCameraScript;
+
+
+    ////////////////////////////////////////////////////////////
+    void Awake()
+    {
+        shipRigidBody = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    ////////////////////////////////////////////////////////////
+    void Update()
     {
 
-        public float speed = 50f;
-        public float turnSpeed = 0.5f;
+        /*powerInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");*/
 
-        public float hoverForce = 65f;
-        public float hoverHeight = 10f;
+        powerInput = fixedJoystick.Vertical;
+        turnInput = fixedJoystick.Horizontal;
 
-        public GameObject laserGreenBullet;
-        public GameObject playerBulletsFolder;
+        speed += powerInput;
 
-        private float maxSpeed = 100;
-        private float minSpeed = 20;
+        if (speed > maxSpeed) { speed = maxSpeed; };
+        if (speed < minSpeed) { speed = minSpeed; };
 
-        private float powerInput;
-        private float turnInput;
+        speedText.text = "Speed: " + Mathf.RoundToInt(speed).ToString();
 
-        public FixedJoystick fixedJoystick;
-
-        private Rigidbody shipRigidBody;
-
-        private Animator anim;
-
-
-        [SerializeField] Text speedText;
-        //[SerializeField] CameraScript myCameraScript;
-
-        
-        ////////////////////////////////////////////////////////////
-        void Awake()
+        if (Input.GetKeyDown("space"))
         {
-            shipRigidBody = GetComponent<Rigidbody>();
-            anim = GetComponentInChildren<Animator>();
+            Shoot();
         }
 
-        ////////////////////////////////////////////////////////////
-        void Update()
+        updateHUD();
+
+        if (healthPoints < 0)
         {
-            powerInput = fixedJoystick.Vertical;
-            turnInput = fixedJoystick.Horizontal;
+            Debug.Log("GAME OVER");
+        }
 
-            speed += powerInput;
-
-            if (speed > maxSpeed) { speed = maxSpeed; };
-            if (speed < minSpeed) { speed = minSpeed; };
-
-            speedText.text = "Speed: " + Mathf.RoundToInt(speed).ToString();
-
-        //float testConvertRange = Utils.convertToNewRange(-100, 100, -50, 50, 90);
-        //Debug.Log(testConvertRange.ToString());
-
-        //if (Input.GetKeyDown("space"))
-        //{
-        //    Shoot();
-        //}
     }
 
 
-        ////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////
 
-        private void FixedUpdate()
+    private void FixedUpdate()
+    {
+    Ray ray = new Ray(transform.position, -transform.up);
+    RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, hoverHeight))
         {
-            Ray ray = new Ray(transform.position, -transform.up);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, hoverHeight))
-            {
-                float proportionalHeight = (hoverHeight - hit.distance) / hoverHeight;
-                Vector3 appliedHoverForce = Vector3.up * proportionalHeight * hoverForce;
-                shipRigidBody.AddForce(appliedHoverForce, ForceMode.Acceleration);
-
-            }
-
-            //
-            //shipRigidBody.AddRelativeForce(0f, 0f, powerInput * speed);
-            shipRigidBody.AddRelativeForce(0f, 0f, speed);
-
-
-            //
-            shipRigidBody.AddRelativeTorque(0f, turnInput * turnSpeed, 0f);
+        float proportionalHeight = (hoverHeight - hit.distance) / hoverHeight;
+        Vector3 appliedHoverForce = Vector3.up * proportionalHeight * hoverForce;
+        shipRigidBody.AddForce(appliedHoverForce, ForceMode.Acceleration);
 
         }
 
-        public void Shoot()
+        shipRigidBody.AddRelativeForce(0f, 0f, speed);
+
+        //
+        shipRigidBody.AddRelativeTorque(0f, turnInput * turnSpeed, 0f);
+
+    }
+
+    public void Shoot()
+    {
+    GameObject bullet = Instantiate(laserGreenBullet, playerBulletsFolder.transform);
+
+        bullet.transform.rotation = transform.rotation;
+
+        if (activeCannon == 1)
         {
-            Debug.Log("player has shot");
-        anim.SetTrigger("ShootLeft");
-        anim.SetTrigger("ShootRight");
-
-        GameObject bullet = Instantiate(laserGreenBullet, playerBulletsFolder.transform);
-
-            bullet.transform.position = transform.position;
-            //bullet.AddRelativeForce(0f, 0, 1000, ForceMode.VelocityChange);
-            bullet.transform.rotation = transform.rotation;
-
-            /*
-            GameObject bullet = ObjectPooler.SharedInstance.GetPooledObject();
-            if (bullet != null)
-            {
-                bullet.transform.position = transform.position;
-                bullet.transform.rotation = transform.rotation;
-            }*/
+            bullet.transform.position = transform.position + new Vector3(2, 0, 0);
+            activeCannon = 2;
         }
+        else if (activeCannon == 2)
+        {
+            bullet.transform.position = transform.position + new Vector3(-2, 0, 0);
+            activeCannon = 1;
+        }
+
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+
+        Destroy(bullet, 3);
+
+    }
 
     public void DodgeLeft()
     {
@@ -123,16 +137,24 @@ using UnityEngine.Serialization;
         anim.SetTrigger("DodgeRight");
         shipRigidBody.AddRelativeForce(50f, 0, 0, ForceMode.Impulse);
     }
+
     ////////////////////////////////////////////////////////////
 
     private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Asteroid" || collision.gameObject.tag == "EnemyShip")
         {
-            if (collision.gameObject.tag == "Asteroid" || collision.gameObject.tag == "EnemyShip")
-            {
-                //myCameraScript.TriggerShake();
-            }
-
+            //myCameraScript.TriggerShake();
         }
+
+    }
+
+    ////////////////////////////////////////////////////////////
+    private void updateHUD()
+    {
+        speedText.text = "Speed: " + Mathf.RoundToInt(speed).ToString();
+        healthText.text = "Health: " + healthPoints.ToString();
+    }
 
 
 }
