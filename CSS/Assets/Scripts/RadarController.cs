ef3d4d3b86203d45b0e2ee1d709dfdd35e1b2f1b
Xavier Agullo <xavier.agullo@socialpoint.es>
2019-08-26 17:18:47 +0200

    Allies in the radar

--- CSS/Assets/Scripts/RadarController.cs
+++ CSS/Assets/Scripts/RadarController.cs
@@ -7,13 +7,15 @@ public class RadarController : MonoBehaviour
 {
 
     public Canvas radarBaseCanvas;
-    public GameObject enemyPointer;
+    public GameObject radarPointer;
 	public GameObject playerShip;
 
     public List<GameObject> enemyPointerList = new List<GameObject>();
+    public List<GameObject> alliesPointerList = new List<GameObject>();
 
-    public Sprite enemyPointerArrowRed;
-    public Sprite enemyPointerArrowYellow;
+    public Sprite radarPointerArrowRed;
+    public Sprite radarPointerArrowYellow;
+    public Sprite radarPointerArrowGreen;
 
 
     private float maxRadarWorldRange = 1000;
@@ -29,8 +31,15 @@ public class RadarController : MonoBehaviour
     {
         foreach (GameObject myEnemyShip in GameManager.Instance.enemyList)
         {
-            GameObject myEnemyPointer = Instantiate(enemyPointer, transform);
+            GameObject myEnemyPointer = Instantiate(radarPointer, transform);
             enemyPointerList.Add(myEnemyPointer);
+        }
+
+        foreach (GameObject myAlliedShip in GameManager.Instance.wingmanList)
+        {
+            GameObject myAlliedPointer = Instantiate(radarPointer, transform);
+            myAlliedPointer.GetComponent<Image>().sprite = radarPointerArrowGreen;
+            alliesPointerList.Add(myAlliedPointer);
         }
     }
 
@@ -40,7 +49,7 @@ public class RadarController : MonoBehaviour
  
         transform.position = new Vector3(playerShip.transform.position.x, 5, playerShip.transform.position.z);
 
-        // Update Pointers
+        // Update Pointers Enemies
         for (int i = 0; i < enemyPointerList.Count; i++)
         {
             // adjust rotation
@@ -63,6 +72,31 @@ public class RadarController : MonoBehaviour
             enemyPointerList[i].gameObject.GetComponent<CanvasGroup>().alpha = alphaAmount;
 
         }
+
+        // Update Pointers Allies
+        for (int i = 0; i < alliesPointerList.Count; i++)
+        {
+            // adjust rotation
+            alliesPointerList[i].transform.rotation = Quaternion.Euler(new Vector3(90, GameManager.Instance.wingmanList[i].transform.eulerAngles.y, 0));
+
+            // adjust position
+            float allyPositionX = GameManager.Instance.wingmanList[i].transform.position.x - playerShip.transform.position.x;
+            float allyPositionY = GameManager.Instance.wingmanList[i].transform.position.z - playerShip.transform.position.z;
+
+            float newXposition = Utils.convertToNewRange(-maxRadarWorldRange, +maxRadarWorldRange, -maxRadarCanvasRange, maxRadarCanvasRange, allyPositionX);
+            float newYposition = Utils.convertToNewRange(-maxRadarWorldRange, +maxRadarWorldRange, -maxRadarCanvasRange, maxRadarCanvasRange, allyPositionY);
+
+            alliesPointerList[i].transform.localPosition = new Vector3(newXposition, 1, newYposition);
+
+            // adjust alpha
+
+            float distanceToPlayer = Mathf.Clamp(Vector3.Distance(GameManager.Instance.wingmanList[i].transform.position, playerShip.transform.position), 0, maxRadarWorldRange);
+            float alphaAmount = pointerMaxAlpha - Utils.convertToNewRange(0, maxRadarWorldRange, 0, pointerMaxAlpha, distanceToPlayer);
+
+            alliesPointerList[i].gameObject.GetComponent<CanvasGroup>().alpha = alphaAmount;
+
+        }
+
     }
 
 
@@ -71,12 +105,12 @@ public class RadarController : MonoBehaviour
     {
         for (int i = 0; i < enemyPointerList.Count; i++)
         {
-            enemyPointerList[i].GetComponent<Image>().sprite = enemyPointerArrowRed;
+            enemyPointerList[i].GetComponent<Image>().sprite = radarPointerArrowRed;
         }
 
         if (enemyID != -1)
         {
-            enemyPointerList[enemyID].GetComponent<Image>().sprite = enemyPointerArrowYellow;
+            enemyPointerList[enemyID].GetComponent<Image>().sprite = radarPointerArrowYellow;
         }
 
     }
